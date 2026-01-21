994da1775dacd472f96b71cc96101c24239458d4
ganeshbajare <bajareganesh9@gmail.com>
2026-01-21 09:59:14 +0530

    commit on 21 jan 2026 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
+// import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
